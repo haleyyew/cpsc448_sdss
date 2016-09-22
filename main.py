5102145d997ca25871e5502bae6c480eb2c88c18
HY <haleyyew@gmail.com>
2016-09-22 08:21:37 -0700

    Small changes: arbitrary join

--- main.py
+++ main.py
@@ -4,10 +4,12 @@ import open_csv
 import table_join
 
 if __name__ == '__main__':
-    ipAll_keys = ['ipAddressString','someOtherID']
+    ipAll_keys = ['ipAddressString','someOtherID','mystatus']
     ipAll_table = open_csv.open_csv_file('ipAll.csv', ipAll_keys)
 
-    ipdomain_keys = ['domainID']
+    ipdomain_keys = ['domainID','status']
     ipdomain_table = open_csv.open_csv_file('ipdomain.csv', ipdomain_keys)
 
-    table_join.table_join(ipAll_table,ipdomain_keys,ipdomain_table,ipdomain_keys)
+    my_join_attr = ['domainID','mystatus']
+    other_join_attr = ['domainID','status']
+    table_join.table_join(ipAll_table,my_join_attr,ipdomain_table,other_join_attr)
--- table_join.py
+++ table_join.py
@@ -33,9 +33,11 @@ def table_join(self_sql_table, my_table_attributes, other_sql_table, other_table
 
     for self_row in self_sql_table.table_rows:
         for other_row in other_sql_table.table_rows:
+
             add = True
             self_row_values = self_sql_table.table_rows[self_row]
             other_row_values = other_sql_table.table_rows[other_row]
+            #print("comparing: ", '|'.join(self_row_values+other_row_values))
             for i in range(len(my_table_attributes_index)):
                 if self_row_values[my_table_attributes_index[i]] == other_row_values[other_table_attributes_index[i]]:
                     continue
